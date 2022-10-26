d6e7b96fdfa53c60358521e8b8c007b1bce44cd8
Ludson96 <ludson_ps27@hotmail.com>
2022-10-26 17:38:02 -0300

    Refatorando desafio 09

--- desafio9.sql
+++ desafio9.sql
@@ -4,5 +4,5 @@ FROM
     SpotifyClone.historico_reproducoes AS his
         INNER JOIN
     SpotifyClone.usuario AS usu ON his.usuario_id = usu.usuario_id
-GROUP BY his.usuario_id
-HAVING his.usuario_id = 1;
+WHERE usu.usuario = 'Barbara Liskov'
+GROUP BY his.usuario_id;
